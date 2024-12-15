19dc669acf30311be6b4f4d37b744a567a9183ac
Andrei Bacin <andreib.pf@gmail.com>
2024-12-15 09:50:19 -0300

    feat: Header section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,14 @@
 import type { Metadata } from "next";
+import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
 
 export const metadata: Metadata = {
   title: "Andrei Bacin | Desenvolvedor Fullstack",
@@ -14,7 +23,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          "antialised bg-gray-900 font-sans text-white",
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,30 @@
+// Header component that renders a navigation bar
 export const Header = () => {
-  return <div>Header Section</div>;
+  return (
+    <div className="relative top-3 flex items-center justify-center">
+      {/* Container for the header, centered with some top margin */}
+      <nav className="flex gap-1 rounded-full border border-white/15 bg-white/10 p-0.5 backdrop-blur">
+        {/* Navigation bar with rounded edges and a blurred background */}
+        {/* Navigation links */}
+        <a href="#" className="nav-item">
+          {/* Link to Home */}
+          Home
+        </a>
+        <a href="#" className="nav-item">
+          {/* Link to Projects */}
+          Projects
+        </a>
+        <a href="#" className="nav-item">
+          {/* Link to About */}
+          About
+        </a>
+        <a
+          href="#" // Link to Contact
+          className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" // Active link with hover effects
+        >
+          Contact
+        </a>
+      </nav>
+    </div>
+  );
 };
